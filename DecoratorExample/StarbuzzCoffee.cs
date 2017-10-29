884b9ef59414d14e307f8d31ccec2cd579f5b67b
Rafael Escoto <rafael.escoto@hotmail.com>
2017-10-29 14:23:21 -0700

    adding project for Decorator Pattern

---
+++ DecoratorExample/StarbuzzCoffee.cs
@@ -0,0 +1,17 @@
+﻿using MyGenerics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DecoratorExample
+{
+    public class StarbuzzCoffee : IInitialStep
+    {
+        public void Start()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- PatternsExamples/Root.cs
+++ PatternsExamples/Root.cs
@@ -3,27 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MyGenerics;
 using DecouplingBehaviors;
 using ObserverExample;
-using MyGenerics;
+using DecoratorExample;
 
 namespace PatternsExamples
 {
     public class Root
     {
-        enum Patterns { Strategy, Observer };
+        enum Patterns { Strategy, Observer, Decorator };
+
         public static void Main() {
 
-            Patterns toPractice = Patterns.Observer;
+            Patterns toPractice = Patterns.Decorator;
 
             switch (toPractice) {
-                case Patterns.Strategy: run(new MiniDuckSimulator()); break;
-                case Patterns.Observer: run(new WeatherMonitoring()); break;
+                case Patterns.Strategy:  run(new MiniDuckSimulator());  break;
+                case Patterns.Observer:  run(new WeatherMonitoring());  break;
+                case Patterns.Decorator: run(new StarbuzzCoffee());     break;
             }
 
             Console.ReadLine();
         }
 
-        public static void run(IInitialStep obj) => obj.Start();
+        public static void run(IInitialStep pattern) => pattern.Start();
     }
 }
