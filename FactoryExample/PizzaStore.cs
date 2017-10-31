0dd6d1fc034e6590bdbd915db5b1e036d0d94b9e
Rafael Escoto <rafael.escoto@hotmail.com>
2017-10-30 21:58:52 -0700

    Factory SetUp

---
+++ FactoryExample/PizzaStore.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyGenerics;
+
+namespace FactoryExample
+{
+    public class PizzaStore : IInitialStep
+    {
+        public void Start()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- PatternsExamples/Root.cs
+++ PatternsExamples/Root.cs
@@ -7,21 +7,23 @@ using MyGenerics;
 using DecouplingBehaviors;
 using ObserverExample;
 using DecoratorExample;
+using FactoryExample;
 
 namespace PatternsExamples
 {
     public class Root
     {
-        enum Patterns { Strategy, Observer, Decorator };
+        enum Patterns { Strategy, Observer, Decorator, Factory };
 
         public static void Main() {
 
             Patterns toPractice = Patterns.Decorator;
 
             switch (toPractice) {
-                case Patterns.Strategy:  run(new MiniDuckSimulator());  break;
-                case Patterns.Observer:  run(new WeatherMonitoring());  break;
-                case Patterns.Decorator: run(new StarbuzzCoffee());     break;
+                case Patterns.Strategy:  run( new MiniDuckSimulator() );  break;
+                case Patterns.Observer:  run( new WeatherMonitoring() );  break;
+                case Patterns.Decorator: run( new StarbuzzCoffee() );     break;
+                case Patterns.Factory:   run( new PizzaStore() );         break;
             }
 
             Console.ReadLine();
